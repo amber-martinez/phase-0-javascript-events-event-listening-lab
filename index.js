18d7dfe64ce4ee985fea5c8ea9293d1bb3b921bb
Amber <martinez.amber1028@gmail.com>
2022-01-07 13:31:04 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+// const firstBox = document.getElementsByTagName('div')[1];
+input = document.getElementById('input');
+
 function addingEventListener() {
+    alert('Congrats, this button was successfully clicked!');
+    input.addEventListener('click', addingEventListener);
 }
+
+addingEventListener()
